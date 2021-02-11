615a551f55a5622521dd1e5a9e3d87eab4aea777
mancabot <78895007+mancabot@users.noreply.github.com>
2021-02-11 12:35:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'MancaGrils'; 
+const BotName = 'GrilsBot'; 
 const instagram = 'https://instagram.com/thenay_xploit_'; 
 const telegram = 'https://t.me/fakboy'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
